8c60891e957e123a10887a7665119577e66823b0
George645 <gsvbutterfly@outlook.com>
2025-02-06 16:14:25 +0000

    VR added to the stuff I have done

---
+++ Assets/scripts/MaterialAdjustments.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class MaterialAdjustments : MonoBehaviour
+{
+    public Material material;
+    bool increasing = true;
+    void Update(){
+        if (increasing) {
+            //material.gri
+        }
+    }
+}
